080307fcb2ef0e34473096f173d8b189f7b942c7
kit28 <ank28.chandrakar@gmail.com>
2026-09-01 13:10:55 +0530

    Create vllm.py

---
+++ vllm.py
@@ -0,0 +1,32 @@
+podman run --name cohere-audio-build \
+  --security-opt=label=disable \
+  -it \
+  --entrypoint /bin/bash \
+  localhost/vllm-openai-cohere:latest
+
+
+root@xxxxx:/workspace#
+
+python -c "import vllm; print(vllm.__version__)"
+
+
+uv pip install --system "vllm[audio]==0.19.0"
+
+
+podman commit cohere-audio-build localhost/vllm-openai-cohere:audio
+
+podman rm cohere-audio-build
+
+
+podman run --rm \
+  --name cohere-asr \
+  --device nvidia.com/gpu=5 \
+  --security-opt=label=disable \
+  --shm-size=16g \
+  -v "/data0/HDD-data/GENAI/cohere-transcribe-arabic-07-2026:/models/cohere:Z" \
+  -p 8006:8000 \
+  localhost/vllm-openai-cohere:audio \
+  --model /models/cohere \
+  --served-model-name cohere-transcribe-arabic-07-2026 \
+  --trust-remote-code
+
